974ba21db36eccc933eb5686f263e5fe517a8f51
DESKTOP-IF8HVDV\Dell <phuongbm@ominext.com>
2021-02-22 20:01:57 +0700

    #2 Finished

---
+++ user/views.py
@@ -0,0 +1,10 @@
+
+
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name= "user/index.html"
+# Create your views here.
+# Create your views here.
